625fdcd5aac42433999c564434dc91412885c4a7
Amit Kumar Pathak <pathakamitkumar67@gmail.com>
2021-03-30 05:34:59 -0700

    switch case

---
+++ switch.js
@@ -0,0 +1,20 @@
+const dice = 4;
+switch(dice){
+    case 1:
+        console.log('you got one');
+        break;
+    case 2:
+        console.log('you got two');
+        break;
+
+    case 3:
+        console.log('you got three');
+        break;
+    case 4:
+        console.log('you got four');
+        break;
+    default:
+        console.log("you do not anythings");
+        break;
+
+}
