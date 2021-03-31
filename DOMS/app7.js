9fe049e03bb4eca576dc00e22d6bb0a85b8d4018
Amit Kumar Pathak <pathakamitkumar67@gmail.com>
2021-03-31 06:16:36 -0700

    CSS

---
+++ DOMS/app7.js
@@ -0,0 +1,7 @@
+
+//css
+const random = document.querySelector('.random');
+random.style.color = 'white';
+random.style.fontSize = '3rem';
+random.style.textTransform = 'capitalize';
+ random.classList.add('title');
