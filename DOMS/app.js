03eb7ad03758960814f9860c8c675911308d150b
Amit Kumar Pathak <pathakamitkumar67@gmail.com>
2021-03-30 23:08:52 -0700

    Basic_DOM

---
+++ DOMS/app.js
@@ -0,0 +1,23 @@
+//similar to CSS
+document.body.style.backgroundColor = 'blue';
+document.body.style.color = 'yellow';
+document.getElementById('btn').style.color = 'red';
+
+//Assign the Variable
+
+const element = document.getElementById('element');
+document.querySelector('element');
+// windows objects
+console.log(window);
+//return a node objects or a node list which is an arraylike objects
+
+const btn = document.getElementById('btn');
+const name =btn.nodeName;
+const css = btn.style;
+console.log(btn);
+console.log(name);
+console.log(css);
+
+
+
+  
